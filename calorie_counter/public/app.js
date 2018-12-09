d9ce1a62d1175d02d3bdb1ec22e306a8d3a3d9ef
prescottbreeden <prescott.breeden@gmail.com>
2018-12-08 17:42:30 -0800

    calorie counter
    drew up html wireframe, added basic styling for development, added an
    event listener for save button on form

---
+++ calorie_counter/public/app.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+  const tableBody = document.getElementById("content");
+
+  const submitBtn = document.getElementById("submit_btn");
+
+  submitBtn.addEventListener('click', function() {
+    console.log('clicked');
+  })
+}
