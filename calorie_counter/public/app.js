5fecd705f32ee6b1f0739427b28ab671c0a9601a
prescottbreeden <prescott.breeden@gmail.com>
2018-12-08 18:47:46 -0800

    calorie counter
    removed placeholders in html, meal data is being imputably rendered on
    page with calorie total, reduced top margin on table

--- calorie_counter/public/app.js
+++ calorie_counter/public/app.js
@@ -1,9 +1,72 @@
 window.onload = () => {
-  const tableBody = document.getElementById("content");
 
+  // data
+  const MEALS = [
+    { description: 'Breakfast', calories: 460 },
+    { description: 'Snack', calories: 100 },
+    { description: 'Lunch', calories: 600 },
+  ];
+
+  // --- FUNCTIONS --- //
+  
+  function cell(value) {
+    const node = document.createElement('td');
+    const data = document.createTextNode(value);
+    node.appendChild(data);
+    return node;
+  }
+
+  function row(meal) {
+    const node = document.createElement('tr');
+    node.appendChild(cell(meal.description));
+    node.appendChild(cell(meal.calories));
+    return node;
+  }
+
+  function getSum(total, num) {
+    return total + num;
+  }
+
+  function addCalories(data) {
+    const arr =[];
+    for (const k of data) {
+      arr.push(k.calories);
+    }
+    return arr.reduce(getSum);
+  }
+
+  function totalRow(data, node) {
+    const total = document.createElement('tr');
+    total.appendChild(cell('TOTAL'));
+    total.appendChild(cell(addCalories(data)));
+    return total;
+  }
+
+  function createTable(data, node) {
+    data.map(meal => {
+      node.appendChild(row(meal));
+    });
+    node.appendChild(totalRow(data, node));
+  }
+
+  // --- GENERATE HTML --- //
+
+  const node = document.getElementById("content");
+  createTable(MEALS, node);
+  
+
+  // --- EVENT LISTENERS --- //
+  
   const submitBtn = document.getElementById("submit_btn");
+  const cancelBtn = document.getElementById("cancel_btn");
+
+  cancelBtn.addEventListener('click', function() {
+    console.log('clicked cancel');
+  });
 
   submitBtn.addEventListener('click', function() {
-    console.log('clicked');
-  })
+    console.log('clicked save');
+  });
+
+
 }
