8201138a464e4cbedf75354ae8b6299bd6e41737
prescottbreeden <prescott.breeden@gmail.com>
2018-12-09 15:54:20 -0800

    counter complete
    added virtual-dom library to counter and replaced strings with object
    variables

--- fpjs/counter/src/index.js
+++ fpjs/counter/src/index.js
@@ -1,15 +1,20 @@
-import hyper from 'hyperscript';
-import hhelper from 'hyperscript-helpers';
+import helpers from 'hyperscript-helpers';
+import { h, diff, patch  } from 'virtual-dom';
+import createElement from 'virtual-dom/create-element';
 
-const { div, button } = hhelper(hyper);
-
+const rootNode = document.getElementById('app');
+const { div, button } = helpers(h);
 const initModel = 0;
+const MSGS = {
+  ADD: 'ADD',
+  SUBTRACT: 'SUBTRACT',
+}
 
 function update(msg, model) {
   switch (msg) {
-    case 'plus':
+    case MSGS.ADD:
       return model + 1;
-    case 'minus':
+    case MSGS.SUBTRACT:
       return model - 1;
     default:
       return model;
@@ -20,9 +25,9 @@ function view(dispatch, model) {
   return div([
     div({ className: 'mv2' }, `Count: ${model}`),
     button({ className: 'pv1 ph2 mr2', 
-      onclick: () => dispatch('plus') }, '+'),
-    button({ className: 'pv1, ph2',
-      onclick: () => dispatch('minus') }, '-'),
+      onclick: () => dispatch(MSGS.ADD) }, '+'),
+    button({ className: 'pv1 ph2',
+      onclick: () => dispatch(MSGS.SUBTRACT) }, '-'),
   ]);
 }
 
@@ -31,17 +36,18 @@ function view(dispatch, model) {
 function app(initModel, update, view, node) {
   let model = initModel; 
   let currentView = view(dispatch, model);
-  node.appendChild(currentView);
+  let rootNode =  createElement(currentView);
+  node.appendChild(rootNode);
 
   function dispatch(msg) {
     model = update(msg, model);
     const updatedView = view(dispatch, model);
-    node.replaceChild(updatedView, currentView);
+    const patches = diff(currentView, updatedView);
+    rootNode = patch(rootNode, patches);
     currentView = updatedView;
   }
 }
 
-const rootNode = document.getElementById('app');
 
 app(initModel, update, view, rootNode);
 
